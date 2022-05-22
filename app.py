5862e77b2360aa6f3aaaa6b192c1498559102527
Alan Alby <alan@rpalabs.io>
2022-05-22 23:53:23 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+
+from flask import Flask, jsonify,request
+# from flask_sqlalchemy import SQLAlchemy
+import os
+import click
+import logging
+
+from flask_migrate import Migrate
+
+from urls.urls import urls
+from models.models import database
+
+
+# from settings import *
+
+
+
+application = Flask(__name__)
+
+logging.basicConfig(filename="logs/error.log",
+                    filemode='a',
+                    format='%(asctime)s %(levelname)s-%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+
+application.debug = True
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+print("In app launch")
+application.register_blueprint(urls)
+
+def page_not_found(e):
+  return jsonify(message='Thanku'), 200
+
+application.register_error_handler(404, page_not_found)
+
+database.init_app(application)
+migrate = Migrate(application, database)
+
+
+if __name__ == '__main__':
+	application.run()
+
+
+
