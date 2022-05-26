ce2f13d88160a113c08fee679040c33258a02efa
Alan Alby <alan@rpalabs.io>
2022-05-26 21:48:52 +0530

    user project added

--- settings.py
+++ settings.py
@@ -1,10 +1,14 @@
+import os
 
+
+ROOT_DIR = os.path.abspath(os.curdir)
+print(ROOT_DIR , "ROOT_DIR")
 
 PYTHON_USER_BACKEND_END_POINT = '/user/get_data'
 
-USER_BACKEND_PROJECT_DIR = "/home/alan/Documents/aerothon/aerothon_python_user_backend"
+USER_BACKEND_PROJECT_DIR = ROOT_DIR + "/user_projects/aerothon_python_user_backend"
 
 FRONT_END_PROJECT_DIR = {
-	"blog" : "/home/alan/Documents/aerothon/airothon_blog_web",
+	"blog" : ROOT_DIR + "/user_projects/airothon_blog_web",
 	"org" : "",
 }
