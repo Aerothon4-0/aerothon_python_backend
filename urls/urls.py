3c4b6e8cc8b0d22c84a9596693d57f6f46e29996
Smitha John <35619971+smithajo@users.noreply.github.com>
2022-05-27 23:38:06 +0530

    Update urls.py

--- urls/urls.py
+++ urls/urls.py
@@ -3,11 +3,12 @@ from flask import Blueprint
 
 from views.views import generate_website
 from views.get_website_data_view import get_website_data
-
+from views.callchatbot import chat_response_message
 
 urls = Blueprint('aerothon', __name__)
 
 # save
 urls.add_url_rule('/code/generate_website', 'generate_website', generate_website, methods=['POST'])
 urls.add_url_rule('/code/get_website_data', 'get_website_data', get_website_data, methods=['GET'])
+urls.add_url_rule('/code/chatbotreply', 'chat_response_message', chat_response_message, methods=['POST'])
 
