02e1fb2fa042ebc11af84ba37b89f9262b08e8f4
Alan Alby <alan@rpalabs.io>
2022-05-27 20:56:03 +0530

    iot backend added

--- user_projects/aerothon_python_user_backend/urls/urls.py
+++ user_projects/aerothon_python_user_backend/urls/urls.py
@@ -1,11 +1,14 @@
 from flask import current_app as app
 from flask import Blueprint
 
-from views.views import get_company_details
+from views.views import get_company_details,update_bulb_status
 
 
 urls = Blueprint('aerothon_user', __name__)
 
 # save
 urls.add_url_rule('/user/get_data', 'get_company_details', get_company_details, methods=['GET'])
+
+# iot
+urls.add_url_rule('/iot/buld', 'update_bulb_status', update_bulb_status, methods=['GET','POST'])
 
--- user_projects/aerothon_python_user_backend/views/views.py
+++ user_projects/aerothon_python_user_backend/views/views.py
@@ -6,7 +6,7 @@ import logging
 import json 
 import uuid
 
-from models.models import Website,database
+from models.models import Website,Bulb,database
 
 
 def get_company_details():
@@ -70,3 +70,80 @@ def get_company_details():
 			})
 
 
+
+
+
+
+def update_bulb_status():
+	if request.method == 'GET':
+		
+		try:
+			function_uuid = uuid.uuid1() 
+			function_name = 'Get bulb status'
+			data = {}
+			#get data
+
+			#get data from database
+			try:
+				bulb = Bulb.query.first()
+				data = {}
+				data['bulb_status'] = bulb.bulb_status
+				print(data , "data")
+				
+			except Exception as e:
+				print("Bulb not found. Create one")
+				bulb_obj = Bulb(
+						bulb_status=True
+					)
+				database.session.add(bulb_obj)
+				database.session.commit()
+				data = {}
+				data['bulb_status'] = True
+
+		except Exception as e:
+			print(e)
+			return jsonify({
+				'error':'true',
+				'message': str(e),
+				'function_name':function_name,
+				'function_uuid':function_uuid,
+			})
+
+
+		return jsonify({
+				'error':'false',
+				'message':'success',
+				'function_name':function_name,
+				'function_uuid':function_uuid,
+				'response_data':data
+			})
+	elif request.method == 'POST':
+		function_uuid = uuid.uuid1() 
+		function_name = 'update bulb status'
+		data = {}
+		#get data
+		try:
+			input_data = json.loads(request.data)
+			bulb_status = input_data.get('bulb_status',False)
+			print(bulb_status , "bulb_status")
+
+		except Exception as e:
+			print(e)
+			app.logger.error('ERROR : %s ',str(e) )
+
+
+		#get data from database
+		try:
+			bulb_obj = Bulb.query.first()
+			bulb_obj.bulb_status = bulb_status
+			database.session.commit()
+		except Exception as e:
+			print(e)
+
+
+		return jsonify({
+				'error':'false',
+				'message':'success',
+				'function_name':function_name,
+				'function_uuid':function_uuid,
+			})
